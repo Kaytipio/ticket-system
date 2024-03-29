4794e68fe6238eac8f01d1e5e6b7aa5ae61d9f5d
Kaytipio <165097667+Kaytipio@users.noreply.github.com>
2024-03-29 18:52:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -64,5 +64,88 @@ async def close(ctx):
         await ctx.send(f"Error to close the ticket: {e}")
 
 
+#Ban members
+            
+@bot.command()
+async def ban(ctx, member : discord.member):
+    if not ctx.author.guild_permissions.ban_members:
+        await ctx.send("You dont have permision sto ban members.")
+        return
+
+    try:
+        await member.ban()
+        await ctx.send(f"{member.name} was banned to the server")
+    except discord.Forbidden:
+        print("An error has occurred")
+
+#Kick members
+        
+@bot.command()
+async def kick(ctx, member : discord.member):
+    if not ctx.author.guild_permissions.kick_members:
+        await ctx.send("You dont have permisions to kick members.")
+        return
+
+    try:
+        await member.kick()
+        await ctx.send(f"{member.name} was kicked to the server")
+    except discord.Forbidden:
+        print("An error has occurred")
+
+
+#Mute members
+
+
+muted_role = "Muted"
+muted_category = "Main" 
+
+@bot.command()
+async def mute(ctx, member:discord.member):
+    if any(role.name in admin_roles for role in ctx.author.roles):
+        role = discord.utils.get(ctx.guild.roles, name=muted_role)
+        if not role:
+            await ctx.send(f"Role {role} not found")
+            return
+        
+        await member.add_roles(role)
+        await ctx.send(f"{member} was muted")
+    else:
+        await ctx.send("You dont have permissions to mute members")
+
+#Unmute members
+        
+@bot.command()
+async def unmute(ctx, member:discord.member):
+    if any(role.name in admin_roles for role in ctx.author.roles):
+        role = discord.utils.get(ctx.guild.roles, name=muted_role)
+        if not role:
+            await ctx.send(f"Role {role} not found")
+            return
+        
+        await member.add_roles(role)
+        await ctx.send(f"{member.mention} was unmuted")
+    else:
+        await ctx.send("You dont have permissions to unmute members")
+
+#Unban members
+        
+@bot.command()
+async def unban(ctx, *, member):
+    if any(role.name in admin_roles for role in ctx.author.roles):
+        banned_users = await ctx.guild.bans()
+        member_name, member_discriminator = member.split('#')
+
+        for ban_entry in banned_users:
+            user = ban_entry.user
+
+            if (user.name, user.discriminator) == (member_name, member_discriminator):
+                await ctx.guild.unban(user)
+                await ctx.send(f'The member {user.mention} has been unbanned.')
+                return
+        await ctx.send(f'Could not find member {member}.')
+    else:
+        await ctx.send("You don't have permission to unban members.")
+
+
 
 bot.run("Your token here")
